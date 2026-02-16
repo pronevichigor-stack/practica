4d2cb246e087a5666ff61f18c053fc4c710d89eb
Игорь Проневич <gr433_prian@lab-402-9.tomtit-tomsk.ru>
2026-02-16 13:00:13 +0700

    Практическая3

--- js/main.js
+++ js/main.js
@@ -2,6 +2,24 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        description: "A pair of warm, fuzzy socks."
+        description: "A pair of warm, fuzzy socks.",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altTexr: "A pair jf socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
