1c873fcd389a05db1f6a9b35d7459067fa04fad4
Игорь Проневич <gr433_prian@lab-402-9.tomtit-tomsk.ru>
2026-02-17 09:52:19 +0700

    шаг 11

--- js/main.js
+++ js/main.js
@@ -53,6 +53,18 @@ Vue.component('product', {
             Delete on cart
             </button>
             <p>Shipping: {{ shipping }}</p>
+            <div>
+            <h2>Reviews</h2>
+            <p v-if="!reviews.length">There are no reviews yet.</p>
+            <ul>
+              <li v-for="review in reviews">
+              <p>{{ review.name }}</p>
+              <p>Rating: {{ review.rating }}</p>
+              <p>{{ review.review }}</p>
+              </li>
+            </ul>
+            </div>
+            <product-review @review-submitted="addReview"></product-review>
         </div>
     </div>
  `,
@@ -84,7 +96,7 @@ Vue.component('product', {
             ],
 
             sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-
+            reviews: []
         }
     },
     methods: {
@@ -98,6 +110,9 @@ Vue.component('product', {
         },
         deleteOnCart() {
             this.$emit('delete-on-cart');
+        },
+        addReview(productReview) {
+            this.reviews.push(productReview)
         }
     },
     computed: {
@@ -122,6 +137,61 @@ Vue.component('product', {
         }
 
     }
+})
+
+Vue.component('product-review', {
+    template: `
+        <form class="review-form" @submit.prevent="onSubmit">
+         <p>
+           <label for="name">Name:</label>
+           <input id="name" v-model="name" placeholder="name">
+         </p>
+        
+         <p>
+           <label for="review">Review:</label>
+           <textarea id="review" v-model="review"></textarea>
+         </p>
+        
+         <p>
+           <label for="rating">Rating:</label>
+           <select id="rating" v-model.number="rating">
+             <option>5</option>
+             <option>4</option>
+             <option>3</option>
+             <option>2</option>
+             <option>1</option>
+           </select>
+         </p>
+        
+         <p>
+           <input type="submit" value="Submit"> 
+         </p>
+        
+        </form>
+
+    `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods:{
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
+        }
+    }
+
+
 })
 
 Vue.component('product-details', {
@@ -150,7 +220,7 @@ let app = new Vue({
             this.cart.push(id);
         },
         deleteCart(id) {
-            this.cart.splice( -1, 1 );
+            this.cart.splice(-1, 1);
         }
     }
 
